f8ba313b12d871b090699fed27696d03ba61b8f7
Vishnu2004-martin <128040550+Vishnu2004-martin@users.noreply.github.com>
2023-08-25 12:51:55 +0530

    Add files via upload

---
+++ RestApi/src/main/java/com/Vishnu/RestApi/day4/model/Model4.java
@@ -0,0 +1,56 @@
+package com.Vishnu.RestApi.day4.model;
+
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="student")
+public class Model4 {student
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	private String studentName;
+	private String departmentName;
+	private String mailid;
+	public Model4() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Model4(int id, String studentName, String departmentName, String mailid) {
+		super();
+		this.id = id;
+		this.studentName = studentName;
+		this.departmentName = departmentName;
+		this.mailid = mailid;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getStudentName() {
+		return studentName;
+	}
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+	public String getDepartmentName() {
+		return departmentName;
+	}
+	public void setDepartmentName(String departmentName) {
+		this.departmentName = departmentName;
+	}
+	public String getMailid() {
+		return mailid;
+	}
+	public void setMailid(String mailid) {
+		this.mailid = mailid;
+	}
+	
+}
---
+++ RestApi/src/main/java/com/Vishnu/RestApi/day4/repository/Repository4.java
@@ -0,0 +1,11 @@
+package com.Vishnu.RestApi.day4.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+import com.Vishnu.RestApi.day4.model.Model4;
+
+
+public interface Repository4 extends JpaRepository<Model4, Integer>{
+
+}
